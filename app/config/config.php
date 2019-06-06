7862245e2589e0a5d1a776093b71be7c1775e24f
Dominik Rusac <rusac.dominik@gmail.com>
2019-06-06 11:48:04 +0200

    Initial commit

---
+++ app/config/config.php
@@ -0,0 +1,18 @@
+<?php
+    // DB params
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', 'dominik');
+    define('DB_NAME', 'shareposts');
+
+    // App Root
+    define('APPROOT', dirname(dirname(__FILE__)));
+    
+    // URL Root
+    define('URLROOT', 'http://localhost/shareposts');
+    
+    // Site Name
+    define('SITENAME', 'SharePosts');
+
+    // App Version
+    define('APPVERSION' , '1.0.0');
